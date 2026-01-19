d29c335445c6739a3531c7000734e5be57a5af7c
persian32 <jhspersian32@gmail.com>
2026-01-19 16:15:04 +0900

    feat: 주사위 게임 구현 (최댓값 갱신 패턴 수집)

--- AA.py
+++ AA.py
@@ -1,30 +1,26 @@
 import sys
-sys.stdin = open("input.txt", "rt") # 파일로 입력받을 때 사용
+sys.stdin=open("input.txt", "rt")
 
-# 패턴 A: 숫자 뒤집기
-def reverse(x):
-    res = 0
-    while x > 0:
-        t = x % 10    # 마지막 자리 숫자 추출
-        res = res * 10 + t
-        x = x // 10   # 마지막 자리 제거
-    return res
+n = int(input())
+res = 0  # 가장 많은 상금을 저장할 변수 (최댓값 갱신용)
 
-# 패턴 B: 소수 판별하기
-def isPrime(x):
-    if x == 1: # 1은 소수가 아님
-        return False
-    # 2부터 자기자신의 절반(또는 제곱근)까지 나누어 떨어지는지 확인
-    for i in range(2, x // 2 + 1):
-        if x % i == 0:
-            return False
-    return True
+for i in range(n):
+    tmp = input().split()
+    tmp.sort() # 정렬을 하면 조건 비교가 훨씬 쉬워집니다.
+    a, b, c = map(int, tmp)
+    
+    # 상금 계산 로직 (조건문 패턴)
+    if a == b and b == c:
+        money = 10000 + a * 1000
+    elif a == b or a == c:
+        money = 1000 + a * 100
+    elif b == c:
+        money = 1000 + b * 100
+    else:
+        money = c * 100 # sort했으므로 c가 가장 큰 눈입니다.
 
-# 메인 실행부
-n = int(input()) # 숫자의 개수 (예: 3)
-a = list(map(int, input().split())) # 숫자 리스트 (예: 32 55 62)
+    # 패턴: 최댓값 갱신하기
+    if money > res:
+        res = money
 
-for x in a:
-    tmp = reverse(x)
-    if isPrime(tmp):
-        print(tmp, end=' ')
+print(res)
